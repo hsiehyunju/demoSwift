3c44e0e34abaf61d13cff9040f178572d3894e60
hsiehyunju <hsiehyunju@gmail.com>
2023-06-15 15:53:55 +0800

    feat: Login View Controller 對應 UI 加入登入功能

--- demoSwift/View Controllers/LoginViewController.swift
+++ demoSwift/View Controllers/LoginViewController.swift
@@ -12,7 +12,18 @@ class LoginViewController : UIViewController {
     
     var viewModel : LoginViewModel!
     
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        if let username = usernameTextField.text, let password = passwordTextField.text {
+            viewModel.signIn(username: username, password: password)
+        }
     }
 }
