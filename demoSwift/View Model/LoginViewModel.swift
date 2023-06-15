6d31177fa8e883fb4dee52a5aac4c28e2d88a49b
hsiehyunju <hsiehyunju@gmail.com>
2023-06-15 10:59:47 +0800

    feat: 建立 Login 和 User 的 ViewModel

---
+++ demoSwift/View Model/LoginViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewModel.swift
+//  demoSwift
+//
+//  Created by hsiehyunju on 2023/6/15.
+//
+
+import Foundation
+
+class LoginViewModel {
+    
+}
---
+++ demoSwift/View Model/UserViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserViewModel.swift
+//  demoSwift
+//
+//  Created by hsiehyunju on 2023/6/15.
+//
+
+import Foundation
+
+class UserViewModel {
+    
+}
