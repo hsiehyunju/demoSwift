231b414661334c041f046ed67dc10c68fcc58794
hsiehyunju <hsiehyunju@gmail.com>
2023-06-15 16:03:28 +0800

    feat: 新增資料更新及更新通知

--- demoSwift/View Model/UserViewModel.swift
+++ demoSwift/View Model/UserViewModel.swift
@@ -8,7 +8,22 @@
 import Foundation
 
 class UserViewModel {
-    
     weak var coordinator : AppCoordinator!
     
+    var userModel: UserModel!
+    var dataUpdated: ((UserModel) -> Void)?
+    
+    func updateUserData() {
+        WebService().update(userModel: userModel, param: ["timezone":8]) { result in
+            do {
+                let updateModel = try result.get()
+                // 更新資料
+                self.userModel.updatedAt = updateModel.updatedAt
+                // 通知資料更新
+                self.dataUpdated?(self.userModel)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
