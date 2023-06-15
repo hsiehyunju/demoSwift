740436546e7ed92a2e1bce7aae36c3744d8e729c
hsiehyunju <hsiehyunju@gmail.com>
2023-06-15 16:33:58 +0800

    Revert "fix: 移除傳遞 Model"
    This reverts commit 30abfc0bda63ad65c2e34a7cdf2c61afb4291ba2.

--- demoSwift/View Model/UserViewModel.swift
+++ demoSwift/View Model/UserViewModel.swift
@@ -26,41 +26,4 @@ class UserViewModel {
             }
         }
     }
-    
-    func getUserName() -> String {
-        return userModel.username
-    }
-    
-    func getEmail() -> String {
-        return userModel.reportEmail
-    }
-    
-    func getEmailVerifiedStatus() -> String {
-        return userModel.isVerifiedReportEmail ? "已驗證" : "未驗證"
-    }
-    
-    func getRegisterTime() -> String {
-        return self.dateFormatter(dateString: userModel.createdAt)
-    }
-    
-    func getUpdateTime() -> String {
-        return self.dateFormatter(dateString: userModel.updatedAt)
-    }
-    
-    private func dateFormatter(dateString: String) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        
-        if let date = dateFormatter.date(from: dateString) {
-            let outputFormatter = DateFormatter()
-            outputFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-            outputFormatter.timeZone = TimeZone.current
-
-            let formattedDate = outputFormatter.string(from: date)
-            return formattedDate
-        } else {
-            print("無法轉換字串")
-            return dateString
-        }
-    }
 }
