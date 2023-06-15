8010f4a3a7e1c3a7356e8b8c74fab651bf0a1a59
hsiehyunju <hsiehyunju@gmail.com>
2023-06-15 16:36:00 +0800

    feat: 資料綁定到 UI 上

--- demoSwift/View Controllers/UserViewController.swift
+++ demoSwift/View Controllers/UserViewController.swift
@@ -12,19 +12,35 @@ class UserViewController : UIViewController {
     
     var viewModel : UserViewModel!
     
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var emailVerifiedTextField: UILabel!
+    @IBOutlet var registerTimeTextField: UITextField!
+    @IBOutlet var dataUpdateTimeTextField: UITextField!
+    @IBOutlet var updateTimezoneButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 更新數據
-        handleDataUpdate(userModel: viewModel.userModel)
+        handleDataUpdate()
         
         // 設定數據更新通知
-        viewModel.dataUpdated = { [weak self] userModel in
-            self?.handleDataUpdate(userModel: userModel)
+        viewModel.dataUpdated = { [weak self] in
+            self?.handleDataUpdate()
         }
     }
     
-    func handleDataUpdate(userModel: UserModel) {
-        
+    func handleDataUpdate() {
+        DispatchQueue.main.async { [self] in
+            self.usernameTextField.text = viewModel.getUserName()
+            self.emailTextField.text = viewModel.getEmailVerifiedStatus()
+            self.emailVerifiedTextField.text = viewModel.getEmailVerifiedStatus()
+            self.registerTimeTextField.text = viewModel.getRegisterTime()
+            self.dataUpdateTimeTextField.text = viewModel.getUpdateTime()
+        }
+    }
+    
+    @IBAction func updateTimezoneButtonTapped(_ sender: Any) {
+        viewModel.updateUserData()
     }
 }
